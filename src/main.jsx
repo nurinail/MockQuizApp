0b6e54df0683af329fffcecee01991bcbaf75ce8
Nail Nuri <nurinail2003@gmail.com>
2025-08-24 18:24:17 +0400

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter  basename={import.meta.env.BASE_URL}>
     <App />
   </BrowserRouter>,
 )
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/MockQuizApp/'
-})
+  // Dev-də '/', build-də '/MockQuizApp/' olsun
+  base: mode === 'production' ? '/MockQuizApp/' : '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'antd-vendor': ['antd'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1200,
+  },
+}))
