4f852edf171789316f95fef0dffc0ddcf08bda43
Nail Nuri <nurinail2003@gmail.com>
2025-08-24 19:41:05 +0400

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  // Dev-də '/', build-də '/MockQuizApp/' olsun
-  base: mode === 'production' ? '/MockQuizApp/' : '/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'antd-vendor': ['antd'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1200,
-  },
+  base:"/MockQuizApp"
 }))
